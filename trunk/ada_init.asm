c1a5940a8ee46dddfa6b7fcdfb1d6daf439065db
pablocarranza@gmail.com <pablocarranza@gmail.com@a2154f4a-1616-e3ab-c018-f13e55a7dae6>
2011-03-05 16:24:05 +0000

    agrego el receiveISR.asm con el handler de interrupcion de SSI, y boludeces

--- trunk/ada_init.asm
+++ trunk/ada_init.asm
@@ -247,97 +247,10 @@ ssi_txls_isr
 ;************************** SSI receive ISR ********************************
 ssi_rxe_isr
         bclr    #5,x:M_SSISR0           ; Read SSISR to clear exception flag
-                                        ; explicitly clears overrun flag
-ssi_rx_isr
-	
-        movep   x:M_RX0,x0         	; Read a/d data
-        move	X:bits,y0
-        jset    #Left_ch,y0,esright 
-        
-;ONSET DETECTION
-		
-		move x0,a	
-
-		abs	a	x0,x:(r0)+	;guardo la muestra en inbuf, y calculo el abs(x(n))
-		
-		move	y:env1,b
-		cmp	b,a				; abs(x(n))>=env(n-1)?
-		
-		bge	envge
-		move	x:decay,x0		;si x<env, hago que env decaiga
-		move	b,y0
-		mpy 	x0,y0,a			;env*decay->env
-		
-envge	move	b,y:env2			; env(n-1)->env2
-		move	a,y:env1			; env(n)->env1
-		
-		
-		move y:ignore,b
-		tst b
-		bne	ignoring
-		
-		move y:env2,b
-		cmp b,a
-		blt nodecrece
-		
-		
-		move y:lastmin,b
-		cmp b,a
-		tlt a,b
-		nop
-		
-		move	b,y:lastmin
-nodecrece	move	y:innote,y0
-		move y:env2,b
-		sub b,a	x:denvt,b
-		cmp b,a	x:envt,b
-		blt	noonset
-		brset	#1,y0,yesonset
-		move y:env1,a
-		cmp b,a	y:lastmin,b
-		blt	noonset
-		sub	b,a	x:mint,b
-		cmp	b,a
-		blt	noonset
-		
-yesonset	bset #1,y:innote
-		move	#0.999,a
-		move a,y:lastmin
-		move #N_IGNORE,a
-		move a,y:ignore
-		move #0.999,a
-		jmp	finiupi
-		
-ignoring	dec b
-		nop
-		move b,y:ignore
-		bra	noend
-noonset	move	y:innote,y0
-		brclr	#1,y0,noend
-		move	y:env1,a
-		move	x:endt,b
-		cmp b,a
-		bge	noend
-endnote	bclr	#1,y:innote
-		move		#0.999,y0
-		move		y0,y:lastmin
-noend	move #0,a
-		nop
-		
-
-;FIN DE ONSET DETECTION
-
-finiupi	move	a,x0
-		jmp	endisr
-	       
-	
-esright move	#0,x0			;mute the other channel
-    
-endisr  movep   x0,x:M_TX00        	; write d/a data
-	bchg	#Left_ch,x:bits
-	rti
-
-
+								; explicitly clears overrun flag
+;ssi_rx_isr
+	include 'receiveISR.asm'
+                                        
 
 ;********************** SSI receive last slot ISR **************************
 ssi_rxls_isr
