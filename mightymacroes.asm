bb05a789d0520dc6652d37b633a1a7561f4c8bba
alejandroluebs@gmail.com <alejandroluebs@gmail.com@a2154f4a-1616-e3ab-c018-f13e55a7dae6>
2011-03-23 06:07:56 +0000

    mulfix con menos error

--- mightymacroes.asm
+++ mightymacroes.asm
@@ -5,14 +5,15 @@ MULFIX		macro			;pisa x0,x1,a . resultado en a y x0
 		bge	_positivo			
 		neg	a		
 		
-		move	#>64,x0	
-		move	a,x1
-		mpy	x0,x1,a
+		rep	#7
+		asl	a
+		
 		neg	a
-		bra	_fin		
-_positivo	move	#>64,x0	
-		move	a,x1
-		mpy	x0,x1,a
+		bra	_fin
+		
+_positivo	rep	#7
+		asl	a
+		
 _fin		move	a0,x0
 		endm
 	
@@ -23,14 +24,15 @@ MULFIXB		macro			;pisa x0,x1,b
 		bge	_positivo			
 		neg	b		
 		
-		move	#>64,y0	
-		move	b,y1
-		mpy	y0,y1,b
+		rep	#7
+		asl	b
+		
 		neg	b
-		bra	_fin		
-_positivo	move	#>64,y0	
-		move	b,y1
-		mpy	y0,y1,b
+		bra	_fin
+			
+_positivo	rep	#7
+		asl	b
+		
 _fin		move	b0,y0
 		endm
 	
